c89af961a19f5dcaa4921fffc4766ce0ae54ce88
argon <argon143@mail.ru>
2023-12-04 04:01:40 +0300

    app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,111 @@
+import styles from './App.module.css';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const fieldsSchema = yup.object().shape({
+	email: yup
+		.string()
+		.required()
+		.matches(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			'Некоректный адрес электронной почты',
+		),
+	password: yup
+		.string()
+		.required()
+		.matches(
+			/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
+			'Пароль должен состоять из не менее 6 символов, содержать латинские буквы верхнего и нижнего регистра, по крайней мере один специальный символ и одну цифру',
+		),
+	repeatPassword: yup
+		.string()
+		.required()
+		.oneOf([yup.ref('password'), null], 'Пароли не совпадают'),
+});
+
+export const App = () => {
+	const [checkMessage, setCheckMessage] = useState();
+
+	const {
+		register,
+		reset,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			repeatPassword: '',
+		},
+		resolver: yupResolver(fieldsSchema),
+	});
+
+	const sendFormData = (formData) => {
+		console.log(formData);
+
+		setCheckMessage('Вы успешно зарегистрированы');
+		reset();
+		setTimeout(setCheckMessage, 3000);
+	};
+
+	const alertMessageEmail = errors.email?.message;
+	const alertMessagePassword = errors.password?.message;
+	const alertMessageRepeatPassword = errors.repeatPassword?.message;
+
+	return (
+		<div className={styles.app}>
+			<section className={styles.firstSection}>
+				{alertMessageEmail && (
+					<div className={styles.alertMessage}>{alertMessageEmail}</div>
+				)}
+				{alertMessagePassword && (
+					<div className={styles.alertMessage}>{alertMessagePassword}</div>
+				)}
+				{alertMessageRepeatPassword && (
+					<div className={styles.alertMessage}>
+						{alertMessageRepeatPassword}
+					</div>
+				)}
+				{checkMessage && (
+					<div className={styles.checkMessage}>{checkMessage}</div>
+				)}
+			</section>
+			<section className={styles.section}>
+				<form onSubmit={handleSubmit(sendFormData)}>
+					<h1 className={styles.divLabelForm}>Введите Ваши данные</h1>
+					<input
+						name="email"
+						type="email"
+						placeholder="Почта"
+						{...register('email')}
+					/>
+					<input
+						name="password"
+						type="password"
+						placeholder="Пароль"
+						{...register('password')}
+					/>
+					<input
+						name="repeatPassword"
+						type="password"
+						placeholder="Повторите пароль"
+						{...register('repeatPassword')}
+					/>
+					<button
+						className={styles.subButton}
+						type="submit"
+						disabled={
+							!!alertMessageEmail ||
+							!!alertMessagePassword ||
+							!!alertMessageRepeatPassword
+						}
+					>
+						Зарегистрироваться
+					</button>
+				</form>
+			</section>
+		</div>
+	);
+};
